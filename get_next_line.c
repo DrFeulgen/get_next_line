be907d1f333054a0a3d0bec9515fbde5a9b10877
Boris BATAINI <bbataini@e1r12p17.42.fr>
2018-02-03 16:59:35 +0100

    rt

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bbataini <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 16:41:59 by bbataini          #+#    #+#             */
-/*   Updated: 2018/01/08 17:27:27 by bbataini         ###   ########.fr       */
+/*   Updated: 2018/02/03 16:36:50 by bbataini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,102 +22,77 @@
    }
    */
 
-char	addmal(char *line, char *save)
+int		addmal(char *save)
 {
-	int i;
 	char *tmp;
 
-	i = 0;
-	tmp = (char *)malloc(sizeof(char) * (ft_strlen(save) + BUF_SIZE));
+	if (!(tmp = (char *)malloc(sizeof(char) * (ft_strlen(save) + 1))))
+		return (0);
 	//	ft_putstr("addma\n");
+	ft_strcpy(tmp, save);
+	save = NULL;
+	if (!(save = ft_strnew(ft_strlen(tmp) + BUF_SIZE)))
+		return(0);
+	ft_strcpy(save, tmp);
+	return (1);
+}
 
-	tmp = line;
-	//	ft_putstr("rth\n");
-	while (save[i])
+int		makeline(int i, char *save, char **line)
+{
+	int x;
+	int a;
+
+	x = 0;
+	a = 0;
+	ft_putstr("call\n");
+	*line = (char *)malloc(sizeof(char) * i);
+	ft_putstr("\n");
+	ft_putstr(save);
+	if (i != 0 || ft_strlen(save) != 0)
 	{
-		tmp[i] = save[i];
-		i++;
-	}
-	i = 0;
-	while (tmp[i + strlen(save)])
-	{
-		if (line[i] == '\n')
+
+		while (save[x] != '\n')
 		{
-			return (tmp[i -1]);
+
+			*line[x] = save[x];
+			x++;
+	ft_putstr(*line);
 		}
-		tmp[i + strlen(save)] = line[i];
-		i++;
+		*line[x] = '\0';
+		x++;
+		while (save[x])
+		{
+			save[a] = save[x];
+			a++;
+			x++;
+		}
+		save[a] = '\0';
 	}
-	//	ft_putstr("tmp\n");
-	ft_putstr(tmp);
-	return (*tmp);
+	return (1);
 }
 
 int		get_next_line(int const fd, char **line)
 {
 	//	char **r;
 	static char *save;
-	char		*str;
-	char		*tmp;
 	int			ret;
 	int			i;
-	int			x;
 
 	i = 0;
-	x = 0;
-	//	save = "";
 	ret = 1;
-	//	ft_putstr("lkj\n");
-	//line = (char **)malloc(sizeof(char *) * 1);
-	//	ft_putstr("ji\n");
-	//		ft_putstr("enter\n");
-	*line = (char *)malloc(sizeof(char) * BUF_SIZE + 1);
-	//		ft_putstr("malloc\n");
+	*line = (char *)malloc(sizeof(char) * BUF_SIZE);
 
 	while ((ret = read(fd, *line, BUF_SIZE)) > 0)
 	{
 		if (!save)
-			save = "";
+		save = "";
+		addmal(save);
 		save = ft_strjoin(save, *line);
-		while (save[i])
-		{
-			if (save[i] == '\n')
-			{
-				str = (char *)malloc(sizeof(char) * i);
-				while (x < i)
-				{
-					str[x] = save[x];
-					x++;
-				}
-				str[x] = '\0';
-				x = ft_strlen(save);
-				tmp = (char *)malloc(sizeof(char) * x - i);
-				x = 0;
-				while (i < x)
-				{
-					tmp[x] = save[x + i];
-					x++;
-				}
-				free(save);
-				save = (char *)malloc(sizeof(char) * ft_strlen(tmp));
-				x = 0;
-				while (tmp[x])
-				{
-					save[x] = tmp[x];
-					x++;
-				}
-				ft_putstr(save);
-				ft_putstr("\0");
-				//ft_putstr(str);
-				free(str);
-				//ft_putstr("save");
-				//ft_putstr(save);
-				ft_putnbr(ret);
-				return (ret);
-			}
-			i++;
-		}
+		makeline(ret, save, &(*line));
+		ft_putnbr(ret);
+		return (ret);
 	}
+	
 	ft_putnbr(ret);
 	return (ret);
 }
